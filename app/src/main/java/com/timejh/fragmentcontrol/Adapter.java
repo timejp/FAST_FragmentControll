7e9ccaef330585fa62e76b6754a89672a83d86ab
timejp <tokijh@naver.com>
2017-02-06 11:24:19 +0900

    Fragment Test

---
+++ app/src/main/java/com/timejh/fragmentcontrol/Adapter.java
@@ -0,0 +1,78 @@
+package com.timejh.fragmentcontrol;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by tokijh on 2017. 2. 1..
+ */
+
+public class Adapter extends RecyclerView.Adapter<Adapter.CustomViewHolder> {
+
+    private final String TAG = "Adapter";
+
+    ArrayList<String> datas;
+    Context context;
+
+    public Adapter(Context context) {
+        datas = new ArrayList<>();
+        this.context = context;
+    }
+
+    public void add(String data) {
+        datas.add(data);
+        this.notifyDataSetChanged();
+    }
+
+    public void set(ArrayList<String> datas) {
+        this.datas = datas;
+        this.notifyDataSetChanged();
+    }
+
+    @Override
+    public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        return new CustomViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(CustomViewHolder holder, int position) {
+        final String data = datas.get(position);
+
+        holder.tv_title.setText(data);
+        holder.position = position;
+
+        Animation animation = AnimationUtils.loadAnimation(context, android.R.anim.fade_in);
+        holder.cardView.setAnimation(animation);
+    }
+
+    @Override
+    public int getItemCount() {
+        return datas.size();
+    }
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tv_title;
+        CardView cardView;
+
+        int position;
+
+        public CustomViewHolder(View itemView) {
+            super(itemView);
+            tv_title = (TextView) itemView.findViewById(R.id.tv_title);
+            cardView = (CardView) itemView.findViewById(R.id.cardView);
+        }
+    }
+}
---
+++ app/src/main/java/com/timejh/fragmentcontrol/BlankFragment.java
@@ -0,0 +1,71 @@
+package com.timejh.fragmentcontrol;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class BlankFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private Adapter adapter;
+    String datas[] = {"월", "화","수","목","금","토","일"};
+
+    private OnFragmentInteractionListener mListener;
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        adapter = new Adapter(getContext());
+        ArrayList<String> datas = new ArrayList<>();
+        for(String data : this.datas) {
+            datas.add(data);
+        }
+        adapter.set(datas);
+        recyclerView.setAdapter(adapter);
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
